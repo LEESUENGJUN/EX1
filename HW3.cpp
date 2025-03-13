c593c90cafab686168ef604e3e5c228740a864eb
LEESUENGJUN <fpemzkdl79@naver.com>
2025-03-13 11:41:56 +0900

    HW3.cpp

--- HW3.cpp
+++ HW3.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n = 5; // 줄 수
+    int n = 5; 
     for (int i = 1; i <= n; i++) {
         for (int space = 1; space <= n - i; space++) {
             cout << "  ";
