07d37eb9e8197ce5a960b83fb98c735be576a2bd
LEESUENGJUN <fpemzkdl79@naver.com>
2025-03-16 20:41:19 +0900

    HW2-1.cpp

--- HW2-1.cpp
+++ HW2-1.cpp
@@ -2,21 +2,19 @@
 
 int main() {
     int num;
-    printf("입력 (0 ~ 99999): ");
+    printf("입력 (0 ~ 9999): ");
     scanf("%d", &num);
 
-    if (num < 0 || num >= 100000) {
+    if (num < 0 || num >= 10000) {
         printf("error\n");
         return 1;
     }
-
-    int m = num / 10000;        
+       
     int t = (num / 1000) % 10;  
     int h = (num / 100) % 10;   
     int s = (num / 10) % 10;    
     int i = num % 10;           
 
-    if (m != 0) printf("%d만 ", m);
     if (t != 0) printf("%d천 ", t);
     if (h != 0) printf("%d백 ", h);
     if (s != 0) printf("%d십 ", s);
