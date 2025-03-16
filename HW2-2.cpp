0b4964c300bc8fed35345f1b7ac96a1e7a13be2a
LEESUENGJUN <fpemzkdl79@naver.com>
2025-03-16 20:53:18 +0900

    Update HW2-2.cpp

--- HW2-2.cpp
+++ HW2-2.cpp
@@ -10,37 +10,30 @@ int main() {
         return 1;
     }
 
-    int t = num / 1000;        
-    int h = (num / 100) % 10;  
-    int s = (num / 10) % 10;   
-    int i = num % 10;          
+    int t = (num / 1000) % 10;  
+    int h = (num / 100) % 10;   
+    int s = (num / 10) % 10;    
+    int i = num % 10;           
 
-    int hasOutput = 0;
+    int hasOutput = 0; 
+    
     if (t != 0) {
-        if (t == 1)
-            printf("천 ");
-        else
-            printf("%d천 ", t);
+        printf("%d천 ", t);
         hasOutput = 1;
     }
     if (h != 0) {
-        if (h == 1)
-            printf("백 ");
-        else
-            printf("%d백 ", h);
+        printf("%d백 ", h);
         hasOutput = 1;
     }
     if (s != 0) {
-        if (s == 1)
-            printf("십 ");
-        else
-            printf("%d십 ", s);
+        printf("%d십 ", s);
         hasOutput = 1;
     }
     if (i != 0) {
         printf("%d", i);
         hasOutput = 1;
     }
+
     if (!hasOutput) {
         printf("0");
     }
